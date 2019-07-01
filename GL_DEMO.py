01042afda39bc703fd8134a98b1090ec3a0a9b79
David Jake Morfe <jakemorfe@gmail.com>
2019-07-01 15:55:24 -0400

    Alpha 1.5

---
+++ GL_DEMO.py
@@ -0,0 +1,14 @@
+from geoliberator import *
+
+##address = input("Enter any address: ")
+##print("0 - Street Name\n1 - AHouse Number\n2 - Full Address")
+##m = int(input("Enter a number 0-2: "))
+##if address == '':
+##    address = "123 Main St, (Default Test Address)"
+##print("Before: ", address, "\nAfter: ", end='')
+m = 2
+address = "22E100ST"
+geoLiberate(address, m)
+
+input("Press Enter to Exit...")
+exit()
