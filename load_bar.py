a2ce948ba830df0ab3f213acaecb599be2d8a0fc
David Jake Morfe <jakemorfe@gmail.com>
2019-07-11 18:03:41 -0400

    Complete

--- load_bar.py
+++ load_bar.py
@@ -17,6 +17,10 @@ class Unbuffered(object):
 
 sys.stdout = Unbuffered(sys.stdout)
 
+def padNum(num):
+    num = str(num)[0:5]
+    return num
+
 def file_len(file_name):
     with open(file_name) as f:
         for i, L in enumerate(f):
@@ -26,14 +30,15 @@ def file_len(file_name):
 file = "new.txt"
 if sys.argv[1] == '--status' or sys.argv[1] == "-S":
     f = open(file); lines = f.readlines(); FL = file_len(file)
-    barIncr = int(FL * .025); barNum = 0; dashNum = 40; c = 0
+    barIncr = int(FL * .025); barNum = 0; dashNum = 40; c = 0; lc = 0
     for line in lines:
-        bar = '|' + ('█' * barNum) + ('-' * dashNum) + '|'
-        c += 1
+        perc = (lc/FL) * 100
+        bar = '|' + ('█' * barNum) + ('-' * dashNum) + '|' + " [" + padNum(perc) + "%]"
+        c += 1; lc += 1
         if c == barIncr:
             if barNum < 39:
                 c = 0; barNum += 1; dashNum -= 1
                 print(f"\r{bar}", end=''); sys.stdout.flush()
                 time.sleep(.025)
-    print('\r|' + ('█' * 40) + '|')
+    print('\r|' + ('█' * 40) + '|' + " [100%]"); sys.stdout.flush()
                 
