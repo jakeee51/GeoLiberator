5b0c2f4a1b6a9d6a9ac50b6faddf4ddf0eaf51a3
David Jake Morfe <jakemorfe@gmail.com>
2019-08-01 16:16:50 -0400

    Initialize

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name = "GeoLiberator",
-    version = "0.2.7",
+    version = "0.2.8",
     author = "David J. Morfe",
     author_email = "jakemorfe@gmail.com",
     license = "MIT",
